b31b2c34fcc958258a2ff2269605a59641c99a6c
N_DK <nguyenkhoadang135759@gmail.com>
2024-08-23 11:04:13 +0700

    update

--- src/models/reportTollboth.model.ts
+++ src/models/reportTollboth.model.ts
@@ -11,10 +11,17 @@ class ReportModel extends DatabaseModel {
 
         offset = parseInt(offset, 10) || 0;
         limit = parseInt(limit, 10) || 99999999;
+
+        // kiểm tra start_date và end_date có cùng tháng không
+        const start = new Date(start_date * 1000);
+        const end = new Date(end_date * 1000);
+
+        const isSameMonth = start.getMonth() === end.getMonth();
+
         const data: any = await new Promise((resolve, rejects) => {
             try {
                 con.query(
-                    `CALL merge_imei_data('${imei}', ${start_date}, ${end_date})`,
+                    `CALL call_merge_imei_data_procedures('${imei}', ${start_date}, ${end_date})`,
                     (err, result) => {
                         if (err) {
                             rejects(err);
@@ -27,10 +34,16 @@ class ReportModel extends DatabaseModel {
             }
         });
 
-        const totalPage = 1;
-        const totalRecord = data[0].length;
+        const dataPage = isSameMonth
+            ? data[0]
+            : Array.isArray(data[1])
+            ? [...data[0], ...data[1]]
+            : data[0];
 
-        const results = data[0].map((item: any) => {
+        const totalRecord = dataPage.length;
+        const totalPage = Math.ceil(totalRecord / limit);
+
+        const results = dataPage.map((item: any) => {
             const { fee, ...rest } = item;
 
             return rest;
@@ -43,25 +56,46 @@ class ReportModel extends DatabaseModel {
         let { offset, limit, imei, start_date, end_date } = query;
         offset = parseInt(offset, 10) || 0;
         limit = parseInt(limit, 10) || 9999999;
-        // call function to get report
-        const data: any = await new Promise((resolve, rejects) => {
-            try {
-                con.query(
-                    `CALL merge_imei_data('${imei}', ${start_date}, ${end_date})`,
-                    (err, result) => {
-                        if (err) {
-                            rejects(err);
-                        }
-                        resolve(result);
-                    },
-                );
-            } catch (error) {
-                rejects(error);
-            }
-        });
-        const totalPage = 1;
-        const totalRecord = data[0].length;
-        return { data: data[0], totalPage, totalRecord };
+
+        const start = new Date(start_date * 1000);
+        const end = new Date(end_date * 1000);
+
+        const isSameMonth = start.getMonth() === end.getMonth();
+
+        try {
+            const data: any = await new Promise((resolve, rejects) => {
+                try {
+                    con.query(
+                        `CALL call_merge_imei_data_procedures('${imei}', ${start_date}, ${end_date})`,
+                        (err, result) => {
+                            if (err) {
+                                rejects(err);
+                            }
+                            resolve(result);
+                        },
+                    );
+                } catch (error) {
+                    rejects(error);
+                }
+            });
+
+            const dataPage = isSameMonth
+                ? data[0]
+                : Array.isArray(data[1])
+                ? [...data[0], ...data[1]]
+                : data[0];
+
+            const totalRecord = dataPage.length;
+            const totalPage = Math.ceil(totalRecord / limit);
+
+            return {
+                data: dataPage.slice(offset, offset + limit),
+                totalPage,
+                totalRecord,
+            };
+        } catch (error: any) {
+            throw new Error(`Error fetching report data: ${error.message}`);
+        }
     }
 }
 
